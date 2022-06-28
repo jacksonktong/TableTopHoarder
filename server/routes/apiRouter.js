5ea5f8a6fd7319e07bb14e43de1d9e8bd6529c90
Jackson Tong <jacksonktong@gmail.com>
2022-06-28 14:38:27 -0700

    Added GET route for usercontroller.

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -4,6 +4,7 @@ const users = require('../models/usersModel.js');
 const authController = require('../controllers/authController.js');
 const { userLogin } = require('../controllers/authController.js');
 const homePageController = require('../controllers/homePageController.js');
+const userController = require('../controllers/userController.js');
 
 router.post('/signup', authController.createUser, (req, res) => {
   return res.status(200).send('success')
@@ -16,5 +17,9 @@ router.post('/login', authController.userLogin, (req, res) => {
 router.get('/home', homePageController.gameOfTheMonth, (req, res) => {
   return res.status(200).send(res.locals.gotm)
 });
+
+router.get('/search', userController.searchInput, (req, res)=> {
+  return res.status(200).send(res.locals.searchResult)
+})
 
 module.exports = router;
