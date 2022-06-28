fcc248d81cabc6c2ba913e16e10e91edfc627aec
Jackson Tong <jacksonktong@gmail.com>
2022-06-27 17:46:22 -0700

    Added homepage to route handler

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const users = require('../models/usersModel.js');
 const authController = require('../controllers/authController.js');
 const { userLogin } = require('../controllers/authController.js');
+const homePageController = require('../controllers/homePageController.js');
 
 router.post('/signup', authController.createUser, (req, res) => {
   return res.status(200).send('success')
@@ -10,6 +11,10 @@ router.post('/signup', authController.createUser, (req, res) => {
 
 router.post('/login', authController.userLogin, (req, res) => {
   return res.status(200).send('success')
+});
+
+router.get('/home', homePageController.gameOfTheMonth, (req, res) => {
+  return res.status(200).send(res.locals.gotm)
 });
 
 module.exports = router;
